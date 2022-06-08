7ae948aa69e99c43ed886f0a6f546ae6d508d3ef
Razvan-DR <64636261+Razvan-DR@users.noreply.github.com>
2022-06-08 05:36:12 +0300

    Update barbut1.0.py

--- barbut1.0.py
+++ barbut1.0.py
@@ -1,37 +1,40 @@
-def winer():
+def winner():  # A recursive function that takes the player name in checks it in the dictionary if found adds 1
     try:
         d[input('W : ')] += 1
-    except Exception as e:
-        print(f"NUMELE >>>  {e}   <<<< NU ESTE VALABIL !!!")
+    except Exception as e:  # Exception for wrong names or names that are not in the original dictionary
+        print(f"NAME >>>  {e}   <<<< Is Not Available !!!")
 
-    s = input('Pres "n" for next turn : / Press "q" to finish the game : ')
+    s = input('\nPres "n" for another turn : / Press "q" to finish the game : ')
     print(" ")
     if s == 'q':
         return d
     else:
-        winer()
+        winner()
 
 
-def sss(n):
+def sss(n):  # Function that evaluates the specific number and returns the correct string for the said relationship
     if n > 0:
-        return("are de primit de la")
+        return("has to receive from")
     elif n < 0:
-        return("are de dat la")
+        return("has to give to ")
     else:
-        return("nu are treaba cu")
+        return("has nothing to do with")
 
 
-def schema(s, s1):
+def schema(s, s1):  # Function that takes each key element(in this program is the name o a player) and established the relationship with the rest of the keys by comparing their values
     q = 0
-    s2 = s1.copy()
+    s2 = s1.copy()  # Makes a copy of the original list i can work on it
     s2.remove(s)
     for i in s2:
 
+        # From the value of the first given key  deduct the value of the second given key and rase it with the stake to obtain the result which is the relationship between the said keys
         r = ((d[s]-d[i])*miza)
 
         print(f"{s} {sss(r)} {s2[q]}", end=" ")
+
         if abs(r) != 0:
-            print(f"{abs(r)} lei")
+            # If r is not 0 print "r $" , else do not print anything
+            print(f"{abs(r)} $")
         else:
             print(" ")
 
@@ -43,10 +46,11 @@ a = int(input('enter the nr of players : '))
 miza = int(input('enter the stake : '))
 d = {}
 
+# Define a dictionary with {a} key-value pairs and takes the key as input and the value is set to zero
 for i in range(a):
     d.update({input('player name : '): 0})
 
-winer()
+winner()
 
 l = list(d.keys())  # Dictionary to list
 i = 0
